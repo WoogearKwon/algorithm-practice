9bbaf35266408c97a11a8bb39cf6113bcbc3e0b9
Woogear Kwon <techkwon1717@hanmail.net>
2020-10-20 06:56:07 +0900

    [EDIT] HashSet에 대한 부가설명 추가

--- src/problems/code_challenge/PickAndPlus.java
+++ src/problems/code_challenge/PickAndPlus.java
@@ -18,7 +18,8 @@ public class PickAndPlus extends Problem {
 
     @Override
     public void run() {
-        int[] numbers = {12,44,3,2,99,7,34,57,43,100,11,53,24,2,1,3,4,1}; // result = [2,3,4,5,6,7]
+        int[] numbers = {12,44,3,2,99,7,34,57,43,100,11,53,24,2,1,3,4,1};
+//        int[] numbers = {2,1,3,4,1}; // result = [2,3,4,5,6,7]
 //        int[] numbers = {5,0,2,7}; // result = [2,5,7,9,12]
         System.out.println(Arrays.toString(solution(numbers)));
     }
@@ -44,6 +45,8 @@ public class PickAndPlus extends Problem {
 
     // 아래는 다른 사람의 풀이
     // 아래는 HashSet을 사용했다.
+    // 나는 HashMap을 사용해서 <key-value>로 저장을 해주었지만,
+    // HashSet은 단순히 객체를 넣어주면 된다. 마찬가지로 중복은 허용하지 않는다.
     public int[] solution2(int[] numbers) {
         Set<Integer> set = new HashSet<>();
 
