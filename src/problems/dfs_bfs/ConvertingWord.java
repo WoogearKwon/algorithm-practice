aa5fa7e73eb3f697a52ab77a8542673533db188b
Woogear Kwon <techkwon1717@hanmail.net>
2020-12-28 23:03:14 +0900

    [EDIT] ConvertingWord 주석 수정

--- src/problems/dfs_bfs/ConvertingWord.java
+++ src/problems/dfs_bfs/ConvertingWord.java
@@ -5,13 +5,20 @@ import problems.Problem;
 /**
  * <단어 변환/>
  * 원문 링크: https://programmers.co.kr/learn/courses/30/lessons/43163?language=java
- * 난이: Level 3
+ * 난이도: Level 3
  *
  * <문제 설명/>
  * 두 개의 단어 begin, target 과 단어의 집합 words 가 있다.
  * 아래와 같은 규칙을 이용해 begin 에서 target 으로 변환하는 가장 짧은 변환 과정을 찾으라.
  * - 한 번에 한 개의 알파벳만 바꿀 수 있다.
  * - words 에 있는 단어로만 변환할 수 있다.
+ *
+ * <제한 사항/>
+ * - 각 단어는 알파벳 소문자로만 이루어져 있습니다.
+ * - 각 단어의 길이는 3 이상 10 이하이며 모든 단어의 길이는 같습니다.
+ * - words에는 3개 이상 50개 이하의 단어가 있으며 중복되는 단어는 없습니다.
+ * - begin과 target은 같지 않습니다.
+ * - 변환할 수 없는 경우에는 0를 return 합니다.
  * */
 public class ConvertingWord extends Problem {
 
