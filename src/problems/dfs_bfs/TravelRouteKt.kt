3ad0560a18b92910864e184939e2fdff65a402a4
Woogear Kwon <techkwon1717@hanmail.net>
2023-08-17 09:45:33 +0900

    [UPDATE] TravelRoute dfs 방식 추가

--- src/problems/dfs_bfs/TravelRouteKt.kt
+++ src/problems/dfs_bfs/TravelRouteKt.kt
@@ -3,10 +3,19 @@ package problems.dfs_bfs
 import problems.Problem
 
 class TravelRouteKt : Problem() {
+    lateinit var answer: List<String>
+    private var visit: BooleanArray = booleanArrayOf()
+
     override fun run() {
         val tickets = arrayOf(arrayOf("ICN", "JFK"), arrayOf("HND", "IAD"), arrayOf("JFK", "HND"))
-        val answer = arrayOf("ICN", "JFK", "HND", "IAD")
+//        mapSolution(tickets)
+        dfsSolution(tickets)
 
+
+        println("answer is ${answer == ANSWER}")
+    }
+
+    private fun mapSolution(tickets: Array<Array<String>>) {
         val map = mutableMapOf<String, String>()
         tickets.forEach {
             map[it[0]] = it[1]
@@ -22,8 +31,41 @@ class TravelRouteKt : Problem() {
             builder.append(",$lastDest")
         }
 
-        val result = builder.toString().split(",")
+        answer = builder.toString().split(",")
         printAnswer(builder.toString())
-        println("answer is ${result == answer}")
+    }
+
+    private val route = mutableListOf<String>()
+
+    private fun dfsSolution(tickets: Array<Array<String>>) {
+        visit = BooleanArray(tickets.size)
+
+        val ticket = tickets[0]
+        val departure = ticket[0]
+        route.add(departure)
+        dfs(1, departure, tickets)
+
+        answer = route
+        printAnswer(route.joinToString(","))
+    }
+
+    private fun dfs(id: Int, destination: String, tickets: Array<Array<String>>) {
+        if (visit[id]) return
+
+        visit[id] = true
+
+        tickets.forEachIndexed { i, ticket ->
+            val dept = ticket[0]
+            val dest = ticket[1]
+
+            if (destination == dept) {
+                route.add(dest)
+                dfs(i, dest, tickets)
+            }
+        }
+    }
+
+    companion object {
+        private val ANSWER = listOf("ICN", "JFK", "HND", "IAD")
     }
 }
