182cbcfc4b46f3362f74adc13e098e1672a3963f
woogear.kwon <techkwon1717@hanmail.net>
2023-08-20 21:49:18 +0900

    [REFACTOR] TargetNumbersKt : 불필요한 함수 제거

--- src/problems/dfs_bfs/TargetNumberKt.kt
+++ src/problems/dfs_bfs/TargetNumberKt.kt
@@ -6,15 +6,10 @@ class TargetNumberKt : Problem() {
 
     override fun run() {
         val numbers = intArrayOf(1, 1, 1, 1, 1)
-        val target = 3 // 기대하는 결과값
-        val answer = 5 // 정답 (5가지)
+        val target = 3
 
-        val result = solution(numbers, target)
+        val result = dfs(0, numbers, 0, target)
         printAnswer(result)
-    }
-
-    private fun solution(numbers: IntArray, target: Int): Int {
-        return dfs(0, numbers, 0, target)
     }
 
     private fun dfs(n: Int, numbers: IntArray, sum: Int, target: Int): Int {
@@ -35,5 +30,14 @@ class TargetNumberKt : Problem() {
                 map { it + i } + map { it - i }
             }
         }.count { it == target }
+    }
+
+    companion object {
+        private const val ANSWER = 5
+        // -1+1+1+1+1 = 3
+        // +1-1+1+1+1 = 3
+        // +1+1-1+1+1 = 3
+        // +1+1+1-1+1 = 3
+        // +1+1+1+1-1 = 3
     }
 }
