bef568daf4c832b314548de79a4600a1cc162889
Woogear Kwon <techkwon1717@hanmail.net>
2023-08-22 12:01:20 +0900

    [UPDATE] GameMapShortestDistance BFS 방식 추가

--- src/problems/dfs_bfs/GameMapShortestDistance.java
+++ src/problems/dfs_bfs/GameMapShortestDistance.java
@@ -3,7 +3,9 @@ package problems.dfs_bfs;
 import problems.Problem;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * [게임 맵 최단거리]
@@ -11,15 +13,11 @@ import java.util.List;
  */
 
 public class GameMapShortestDistance extends Problem {
-    private int distance = 0;
-    private int[][] maps;
-    private boolean[][] visited;
-
     @Override
     public void run() {
         Case testCase = cases.get(1);
 
-        int result = solution1(testCase.maps);
+        int result = solution2(testCase.maps);
         int answer = testCase.answer;
 
         printResult(result);
@@ -27,13 +25,56 @@ public class GameMapShortestDistance extends Problem {
     }
 
     /**
-     * 아래의 방법으로 문제 풀이는 가능하나 효율성 테스트에서 4개 모두 시간초과로 실패
-     * */
+     * BFS 풀이방식
+     */
+    public int solution2(int[][] maps) {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {-1, 1, 0, 0};
+        int[][] distances = new int[maps[0].length][maps.length];
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        distances[0][0] = 1;
+
+        while (!q.isEmpty()) {
+            int[] point = q.poll();
+            int x = point[0];
+            int y = point[1];
+
+            for (int i = 0; i < dx.length; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx > maps[0].length - 1 || ny > maps.length - 1) {
+                    continue;
+                }
+
+                if (distances[nx][ny] == 0 && maps[ny][nx] == 1) {
+                    distances[nx][ny] = distances[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int distance = distances[maps[0].length - 1][maps.length - 1];
+
+        return distance == 0 ? -1 : distance;
+    }
+
+    /**
+     * 아래의 방법(DFS)으로 문제 풀이는 통과했으나 효율성 테스트에서 4개 모두 시간초과로 실패
+     * 최단거리를 구하는 문제이므로 DFS 보다 BFS가 적절
+     */
+    private int distance = 0;
+    private int[][] maps;
+    private boolean[][] visited;
+
     public int solution1(int[][] maps) {
         this.maps = maps;
+
         visited = new boolean[maps.length][maps[0].length];
 
-        dfs1(1, 0, 0, maps.length - 1, maps[0].length - 1);
+        dfs(1, 0, 0, maps.length - 1, maps[0].length - 1);
 
         if (distance == 0) {
             return -1;
@@ -42,12 +83,10 @@ public class GameMapShortestDistance extends Problem {
         }
     }
 
-    private void dfs1(int count, int x, int y, int mX, int mY) {
+    private void dfs(int count, int x, int y, int mX, int mY) {
         if (x < 0 || y < 0 || x > mX || y > mY) return;
         if (maps[x][y] == 0) return;
         if (visited[x][y]) return;
-
-        System.out.println("dfs,,count = " + count + "..." + x + "," + y);
 
         if (x == mX && y == mY) {
             distance = (distance == 0) ? count : Math.min(distance, count);
@@ -55,10 +94,10 @@ public class GameMapShortestDistance extends Problem {
         }
 
         visited[x][y] = true;
-        dfs1(count + 1, x - 1, y, mX, mY);
-        dfs1(count + 1, x + 1, y, mX, mY);
-        dfs1(count + 1, x, y - 1, mX, mY);
-        dfs1(count + 1, x, y + 1, mX, mY);
+        dfs(count + 1, x - 1, y, mX, mY);
+        dfs(count + 1, x + 1, y, mX, mY);
+        dfs(count + 1, x, y - 1, mX, mY);
+        dfs(count + 1, x, y + 1, mX, mY);
         visited[x][y] = false;
     }
 
