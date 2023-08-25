40d77d81f2523e3584a5b5fd67cc8f0d1451b1a3
woogear.kwon <techkwon1717@hanmail.net>
2023-08-25 22:25:49 +0900

    [ADD] BestAlbumKt 추가

--- src/Main.java
+++ src/Main.java
@@ -41,42 +41,43 @@ public class Main {
         problems.put(7, new Marathon());
         problems.put(8, new PhoneNumbers());
         problems.put(9, new Camouflage());
-
         problems.put(10, new BestAlbum());
-        problems.put(11, new StockPrice());
-        problems.put(12, new DevelopingFunction());
-        problems.put(13, new PassingTrucks());
-        problems.put(14, new Printer());
-        problems.put(15, new MoreSpicy());
-        problems.put(16, new DiskController());
-        problems.put(17, new DoublePriorityQueue());
-        problems.put(18, new PracticeExam());
-        problems.put(19, new FindingPrime());
 
-        problems.put(20, new Carpet());
-        problems.put(21, new TargetNumber());
-        problems.put(22, new TargetNumberKt());
-        problems.put(23, new ConvertingWordsKt());
-        problems.put(24, new Network());
-        problems.put(25, new ConvertingWord());
-        problems.put(26, new TravelRoute());
-        problems.put(27, new TravelRouteKt());
-        problems.put(28, new KeypadClickKt());
-        problems.put(29, new PickAndPlusKt());
+        problems.put(11, new BestAlbumKt());
+        problems.put(12, new StockPrice());
+        problems.put(13, new DevelopingFunction());
+        problems.put(14, new PassingTrucks());
+        problems.put(15, new Printer());
+        problems.put(16, new MoreSpicy());
+        problems.put(17, new DiskController());
+        problems.put(18, new DoublePriorityQueue());
+        problems.put(19, new PracticeExam());
+        problems.put(20, new FindingPrime());
 
-        problems.put(30, new HouseOneKt());
-        problems.put(31, new HouseTwoKt());
-        problems.put(32, new NetworkKt());
-        problems.put(33, new Fibonacci());
-        problems.put(34, new NewAverageKt());
-        problems.put(35, new RemainingSumKt());
-        problems.put(36, new SequenceNumberSumKt());
-        problems.put(37, new FurthestNode());
-        problems.put(38, new ConnectedComponents());
-        problems.put(39, new GameMapShortestDistance());
+        problems.put(21, new Carpet());
+        problems.put(22, new TargetNumber());
+        problems.put(23, new TargetNumberKt());
+        problems.put(24, new ConvertingWordsKt());
+        problems.put(25, new Network());
+        problems.put(26, new ConvertingWord());
+        problems.put(27, new TravelRoute());
+        problems.put(28, new TravelRouteKt());
+        problems.put(29, new KeypadClickKt());
+        problems.put(30, new PickAndPlusKt());
 
-        problems.put(40, new BoxerRank());
-        problems.put(41, new Pokemon());
+        problems.put(31, new HouseOneKt());
+        problems.put(32, new HouseTwoKt());
+        problems.put(33, new NetworkKt());
+        problems.put(34, new Fibonacci());
+        problems.put(35, new NewAverageKt());
+        problems.put(36, new RemainingSumKt());
+        problems.put(37, new SequenceNumberSumKt());
+        problems.put(38, new FurthestNode());
+        problems.put(39, new ConnectedComponents());
+        problems.put(40, new GameMapShortestDistance());
+
+        problems.put(41, new BoxerRank());
+        problems.put(42, new Pokemon());
         // 가장 최근에 추가된 문제 실행
         problems.get(problems.size() - 1).run();
     }
---
+++ src/problems/hash/BestAlbumKt.kt
@@ -0,0 +1,74 @@
+package problems.hash
+
+import problems.Problem
+
+class BestAlbumKt : Problem() {
+
+    override fun run() {
+        val testCase = cases[0]
+        val result = solution(testCase.genres, testCase.plays)
+        val answer = testCase.answer
+
+        printResult(result)
+        println("Your answer is ${result.contentEquals(answer)}")
+    }
+
+    fun solution(genres: Array<String>, plays: IntArray): IntArray {
+        val map = mutableMapOf<String, Int>()
+        val musics = mutableListOf<Music>()
+
+        for (i in genres.indices) {
+            val playCount = map[genres[i]] ?: 0
+            map[genres[i]] = playCount + plays[i]
+            musics.add(Music(i, genres[i], plays[i]))
+        }
+
+        val sortedMap = map
+            .toList()
+            .sortedBy { (_, value) -> value }
+            .reversed()
+            .toMap()
+
+        val result = mutableListOf<Int>()
+        sortedMap.keys.forEach { genre ->
+            val filteredMusics = musics
+                .filter { it.genre == genre }
+                .sortedBy { it.id }
+                .reversed()
+                .sortedBy { it.playCount }
+                .reversed()
+
+            result.add(filteredMusics.first().id)
+            if (filteredMusics.size > 1) result.add(filteredMusics[1].id)
+        }
+
+        return result.toIntArray()
+    }
+
+    private data class Music(
+        val id: Int,
+        val genre: String,
+        val playCount: Int,
+    )
+
+    companion object {
+        private val cases = listOf(
+            Case(
+                genres = arrayOf("classic", "pop", "classic", "classic", "pop"),
+                plays = intArrayOf(500, 600, 150, 800, 2500),
+                answer = intArrayOf(4, 1, 3, 0)
+            ),
+            Case(
+                genres = arrayOf("classic", "pop", "classic", "classic", "pop"),
+                plays = intArrayOf(500, 600, 500, 800, 2500),
+                answer = intArrayOf(4, 1, 3, 0)
+            )
+        )
+    }
+
+    private class Case(
+        val genres: Array<String>,
+        val plays: IntArray,
+        val answer: IntArray
+    )
+}
